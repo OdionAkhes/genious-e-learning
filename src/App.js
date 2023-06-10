2be8114bacfd0062008452068d0ee552bb16e528
Odion Akhes <akhetuamhenodion@gmail.com>
2023-06-10 08:07:10 +0100

    Setting up pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      klmmllm'l'
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/CourseDetailsContent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CourseDetailsContent = () => {
+  return (
+    <div>CourseDetailsContent</div>
+  )
+}
+
+export default CourseDetailsContent
